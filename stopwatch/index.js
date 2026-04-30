e43f678d1c0a2023f1732d464df9abc78ff55d70
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-04-30 18:21:47 +0530

    stopwatch updated

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -1,16 +1,39 @@
-let intervalId;
-let time = 0;
-document.querySelector(".watch").innerHTML = time;
+let [seconds, minutes, hours] = [0, 0, 0];
+let displayElem = document.querySelector(".display");
+let buttonElems = document.querySelector(".bttns")
+let timer = null;
 
-function srtwatch() {
-    let inputElement = document.querySelector(".watch");
-    inputElement.innerHTML = ++time;
+function watch() {
+    seconds++
+    if(seconds == 60) {
+        seconds = 0;
+        minutes++;
+        if(minutes == 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+let hrs = hours<10? "0" + hours : hours;
+let mins = minutes<10? "0" + minutes : minutes;
+let secs = seconds<10? "0" + seconds : seconds;
+
+    // console.log(hours + ":" + minutes + ":" + seconds )
+    displayElem.innerHTML = hrs + ":" + mins + ":" + secs ;
+};
+
+function start () {
+    if (timer !== null) {
+        clearInterval(timer);
+    }
+   timer = setInterval(watch, 1000);
 }
 
 function stop() {
-    clearInterval(intervalId);
+    clearInterval(timer);
 }
 
-function start() {
-    intervalId = setInterval(srtwatch, 1000)
+function reset () {
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0, 0, 0];
+    displayElem.innerHTML= "00:00:00"
 }
