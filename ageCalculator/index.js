5fd13c47a6625316ec891a4ae238d3307c40a102
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-05-03 21:50:04 +0530

    made an age calculator

---
+++ ageCalculator/index.js
@@ -0,0 +1,41 @@
+let userInput = document.querySelector("#date");
+userInput.max = new Date().toISOString().split("T")[0];
+
+function calculateAge() {
+    let birthDate = new Date(userInput.value);
+
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth() + 1;
+    let y1 = birthDate.getFullYear();
+
+    let today = new Date();
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+    
+
+    let y3, m3, d3;
+
+    y3 = y2 - y1;
+
+    if(m2 >= m1) {
+        m3 = m2 - m1;
+    } else {
+        y3--;
+        m3 = 12 + m2 - m1;
+    } 
+    if (d2 >= d1) {
+        d3 = d2 - d1;
+    } else {
+        m3--
+        d3 = getDaysInMonth(y1, m1) + d2 - d1
+    }
+
+    let result = document.querySelector(".result");
+    result.innerHTML = "Age is " + y3 + " years " + m3 + " months " + d3 + " days "
+}
+
+function getDaysInMonth (year, month) {
+    return new Date(year, month, 0).getDate()
+}
